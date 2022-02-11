96e0e7afbc2639b06bda7a76982b97d2646f9084
vickykanse <vickykanse149@gmail.com>
2022-02-11 11:26:26 +0530

    re install fullpage

--- src/Components/Homepage.js
+++ src/Components/Homepage.js
@@ -4,7 +4,8 @@ import AboutCompany from './homepagecomponent/aboutcompany/aboutcompanyindex';
 import Ourservice from './homepagecomponent/ourservice/ourservice';
 import Portfolio from './homepagecomponent/our_portfolio/our_portfolio';
 import Ourclient from './homepagecomponent/ourclient/ourclient';
-
+import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
+import ReactFullpage from "@fullpage/react-fullpage";
 import Footer from './footer/footer';
 // import "fullpage.js/vendors/scrolloverflow"; // Optional. When using scrollOverflow:true
 // import ReactFullpage from "@fullpage/react-fullpage";
@@ -17,7 +18,12 @@ class Homepage extends Component {
     render() {
         // const anchors = ["0", "1", "2", "3", "4", "5"];
       return (
-        <div id="fullpage-wrapper">
+        <ReactFullpage
+        licenseKey={"YOUR_LICENSE_KEY"}
+        scrollOverflow={true}
+        render={({ state, fullpageApi }) => {
+          return (
+            <div id="fullpage-wrapper">
         <div className="hero section fp-section-0">
             <Section1 />
             {/* <button className="scroll" onClick={() => fullpageApi.moveSectionDown()}>
@@ -27,22 +33,30 @@ class Homepage extends Component {
                 </div>
             </button> */}
         </div>
+        
         <section className="section home-about fp-section-1">
             <AboutCompany />
         </section>
+        
         <section className="our-services section  fp-section-2">
             <Ourservice />
         </section>
+       
         <section className="cm_portfolio_main section  fp-section-3">
             <Portfolio />
         </section>
+       
         <section className="cm_our_clients section  fp-section-4">
             <Ourclient />
         </section>
+      
         <footer className="section footer-main active inner-page-footer">
             <Footer></Footer>
         </footer>
-      </div>
+        </div>
+        );
+    }}
+  />
       );
     }
   }
